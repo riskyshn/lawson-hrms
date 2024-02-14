24d40ae5355391a4f232d3c794496024228ca2e2
Dede ardiansya <me@dedeard.my.id>
2024-02-14 13:42:39 +0800

    ✨feature: setup tailwind.

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+import colors from "tailwindcss/colors";
+
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    container: {
+      center: true,
+    },
+    extend: {
+      colors: {
+        primary: colors.blue,
+        secondary: colors.gray,
+        success: colors.green,
+        warning: colors.yellow,
+        error: colors.red,
+      },
+    },
+  },
+  plugins: [],
+};
