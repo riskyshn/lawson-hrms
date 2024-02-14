d85e336d4fb0e3808d36a8df43ccc25d8adf64ba
Dede ardiansya <me@dedeard.my.id>
2024-02-14 13:53:03 +0800

    ✨feature: setup project packages.

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,7 @@ import colors from 'tailwindcss/colors'
 
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}', './node_modules/jobseeker-ui/src/components/libs/**/*.{js,ts,jsx,tsx}'],
   theme: {
     container: {
       center: true,
