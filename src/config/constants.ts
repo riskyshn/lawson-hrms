95949b76862ce9b60c5c54e056fbcef687211fdc
Dede ardiansya <me@dedeard.my.id>
2024-02-19 14:33:42 +0800

    ✨feature: create token store.

---
+++ src/config/constants.ts
@@ -0,0 +1,4 @@
+export const ACCESS_TOKEN_KEY = 'access-token'
+export const REFRESH_TOKEN_KEY = 'refresh-token'
+export const ACCESS_TOKEN_EXPIRATIONS = 6 // Hours
+export const REFRESH_TOKEN_EXPIRATIONS = 24 * 7 // Hours (7 days)
