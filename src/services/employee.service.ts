ecac6679b6fe12d7181a9219304f15f2917b42e6
Dede ardiansya <me@dedeard.my.id>
2024-03-18 21:59:16 +0800

    ✨feature: setup employee service.

--- src/services/employee.service.ts
+++ src/services/employee.service.ts
@@ -12,6 +12,22 @@ const axios = createAxiosInstance({
  * Employee
  *
  */
-export const fetchEmployees = (params?: IPaginationParam, signal?: GenericAbortSignal) => {
+export const fetchEmployees = (params?: IPaginationParam & { status?: 1 | 2 }, signal?: GenericAbortSignal) => {
   return axios.get<{ data: IPaginationResponse<IEmployee> }>(`/employee`, { params, signal }).then((response) => response.data.data)
+}
+
+export const fetchEmployee = (oid: string) => {
+  return axios.get<{ data: IEmployee }>(`/employee/${oid}`).then((response) => response.data.data)
+}
+
+export const createEmployee = (payload: Record<string, any>) => {
+  return axios.post<{ data: IEmployee }>(`/employee`, payload).then((response) => response.data.data)
+}
+
+export const updateEmployee = (id: string, payload: Record<string, any>) => {
+  return axios.put<{ data: IEmployee }>(`/employee/${id}`, payload).then((response) => response.data.data)
+}
+
+export const deleteEmployee = (id: string) => {
+  return axios.delete(`/employee/${id}`).then((response) => response.data.data)
 }
