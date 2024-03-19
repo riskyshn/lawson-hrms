104a3fab85c3889f12a6addc4a891cae342bdeb8
Hafizh Pratama <hafizhpratama99@gmail.com>
2024-03-19 16:35:46 +0800

    feature: add process

---
+++ src/pages/process/assessment/create/CreateProcessPage.tsx
@@ -0,0 +1,110 @@
+import Container from '@/components/Elements/Container'
+import PageHeader from '@/components/Elements/PageHeader'
+import { Button, Stepper, useSteps, useToast } from 'jobseeker-ui'
+import { Link, useNavigate } from 'react-router-dom'
+import { useState } from 'react'
+import KTPForm from '../components/KTPForm'
+import NPWPForm from '../components/NPWPForm'
+import KKForm from '../components/KKForm'
+import PaklaringForm from '../components/PaklaringForm'
+import PaymentSlipForm from '../components/PaymentSlipForm'
+
+const CreateProcessPage = () => {
+  const [isSubmitLoading, setIsSubmitLoading] = useState(false)
+  const navigate = useNavigate()
+  const toast = useToast()
+
+  const [formValues, setFormValues] = useState<any>({
+    personalData: {},
+    employmentData: {},
+    payrollData: {},
+    componentsData: {},
+  })
+
+  const { activeStep, isLastStep, handlePrev, handleNext } = useSteps(5, {
+    onNext() {
+      window.scrollTo({ top: 0, behavior: 'smooth' })
+    },
+  })
+
+  const handleStepSubmit = async (data: any) => {
+    setFormValues(data)
+    handleNext()
+
+    if (!isLastStep) return
+
+    try {
+      setIsSubmitLoading(true)
+      toast('Assessment process successfully created.', { color: 'success', position: 'top-right' })
+      navigate(`/process/assessment`)
+    } catch (error) {
+      toast('An error occurred while creating the assessment process.', { color: 'error', position: 'top-right' })
+      setIsSubmitLoading(false)
+    }
+  }
+
+  return (
+    <>
+      <PageHeader
+        breadcrumb={[{ text: 'Process' }, { text: 'Assesment' }, { text: 'Process' }]}
+        title="Upload Document"
+        subtitle="Company Name requires you to upload your documents inorder to complete the hiring process"
+        actions={
+          <Button as={Link} to="/process/assessment" variant="light" color="error">
+            Cancel
+          </Button>
+        }
+      />
+      <Container className="flex flex-col gap-3 py-3 xl:pb-8">
+        <Stepper
+          activeStep={activeStep}
+          steps={[
+            { title: 'KTP', details: 'Kartu Identitas Penduduk' },
+            { title: 'NPWP', details: 'Nomor Pokok Wajib Pajak' },
+            { title: 'KK', details: 'Kartu Keluarga' },
+            { title: 'Paklaring', details: 'Surat Keterangan Kerja' },
+            { title: 'Payment Slip', details: 'Slip Gaji' },
+          ]}
+        />
+
+        {activeStep === 0 && (
+          <KTPForm
+            defaultValue={formValues.personalData}
+            handlePrev={handlePrev}
+            handleSubmit={(ktpData) => handleStepSubmit({ ...formValues, ktpData })}
+          />
+        )}
+        {activeStep === 1 && (
+          <NPWPForm
+            defaultValue={formValues.employmentData}
+            handlePrev={handlePrev}
+            handleSubmit={(npwpData) => handleStepSubmit({ ...formValues, npwpData })}
+          />
+        )}
+        {activeStep === 2 && (
+          <KKForm
+            defaultValue={formValues.payrollData}
+            handlePrev={handlePrev}
+            handleSubmit={(kkData) => handleStepSubmit({ ...formValues, kkData })}
+          />
+        )}
+        {activeStep === 3 && (
+          <PaklaringForm
+            defaultValue={formValues.payrollData}
+            handlePrev={handlePrev}
+            handleSubmit={(paklaringData) => handleStepSubmit({ ...formValues, paklaringData })}
+          />
+        )}
+        {activeStep === 4 && (
+          <PaymentSlipForm
+            defaultValue={formValues.payrollData}
+            handlePrev={handlePrev}
+            handleSubmit={(paymentSlipData) => handleStepSubmit({ ...formValues, paymentSlipData })}
+          />
+        )}
+      </Container>
+    </>
+  )
+}
+
+export default CreateProcessPage
---
+++ src/pages/process/offering-letter/index/OfferingLetterPage.tsx
@@ -0,0 +1,68 @@
+import Container from '@/components/Elements/Container'
+import MainCard from '@/components/Elements/MainCard'
+import PageHeader from '@/components/Elements/PageHeader'
+import usePagination from '@/hooks/use-pagination'
+import { BaseInput, Button, Select } from 'jobseeker-ui'
+import { FilterIcon, SearchIcon } from 'lucide-react'
+import Table from '../components/Table'
+import { Link } from 'react-router-dom'
+
+const OfferingLetterPage: React.FC = () => {
+  const pagination = usePagination({ pathname: '/process/offering-letter', totalPage: 2, params: { search: 'querysearch' } })
+
+  return (
+    <>
+      <PageHeader
+        breadcrumb={[{ text: 'Process' }, { text: 'Offering Letter' }]}
+        title="Offering Letter"
+        actions={
+          <>
+            <Button as={Link} to="/process/offering-letter/create" color="primary" className="ml-3">
+              Setup Offering Letter
+            </Button>
+          </>
+        }
+      />
+
+      <Container className="relative flex flex-col gap-3 py-3 xl:pb-8">
+        <MainCard
+          header={(open, toggleOpen) => (
+            <>
+              <div className="flex flex-col gap-3 p-3 lg:flex-row lg:items-center lg:justify-between">
+                <div>
+                  <div className="mb-2">
+                    <span className="block text-lg font-semibold">Candidate List</span>
+                    <span className="block text-sm">
+                      You have <span className="text-primary-600">You have 21000 Candidates in total</span> in total
+                    </span>
+                  </div>
+                </div>
+                <div className="flex items-center gap-2">
+                  <div className="relative flex flex-1">
+                    <BaseInput type="text" placeholder="Search..." className="peer w-full rounded-r-none lg:w-64" />
+                    <Button iconOnly color="primary" className="rounded-l-none">
+                      <SearchIcon size={16} />
+                    </Button>
+                  </div>
+                  <Button iconOnly type="button" color="primary" onClick={toggleOpen}>
+                    <FilterIcon size={16} />
+                  </Button>
+                </div>
+              </div>
+              {open && (
+                <div className="grid grid-cols-2 gap-3 p-3">
+                  <Select placeholder="All Vacancy" options={[]} />
+                  <Select placeholder="All Stage" options={[]} />
+                </div>
+              )}
+            </>
+          )}
+          body={<Table />}
+          footer={pagination.render()}
+        />
+      </Container>
+    </>
+  )
+}
+
+export default OfferingLetterPage
