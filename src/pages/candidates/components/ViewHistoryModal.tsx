e89f9a3ead8de7e69ac1d2bb729d9b6ed8c6d173
Dede ardiansya <me@dedeard.my.id>
2024-03-28 15:57:11 +0800

    ✨feature: complete history modal.

--- src/pages/candidates/components/ViewHistoryModal.tsx
+++ src/pages/candidates/components/ViewHistoryModal.tsx
@@ -1,187 +1,174 @@
-import React, { useState } from 'react'
-import { Button, Card, CardBody } from 'jobseeker-ui'
 import MainModal from '@/components/Elements/MainModal'
-import { CalendarIcon, CheckCircle2Icon, FileInputIcon, UserIcon, XCircleIcon } from 'lucide-react'
+import { processService } from '@/services'
+import { Button, Card, CardBody, Spinner } from 'jobseeker-ui'
+import {
+  AlertCircleIcon,
+  CalendarIcon,
+  CheckCircle2Icon,
+  CircleEllipsisIcon,
+  ClockIcon,
+  FileInputIcon,
+  LucideIcon,
+  UserIcon,
+  XCircleIcon,
+} from 'lucide-react'
+import moment from 'moment'
+import React, { useEffect, useState } from 'react'
+import { twJoin } from 'tailwind-merge'
 
 type OptionModalProps = {
-  show: boolean
-  onClose: () => void
-  candidate: any
+  show?: boolean
+  candidate?: ICandidate
+  onClose?: () => void
 }
 
-const data = [
-  {
-    candidate: {
-      name: 'John Doe',
-    },
-    jobApplication: {
-      id: '#RR00000041373',
-      position: 'Sales Promotion',
-    },
-    jobApplicationProcess: [
-      {
-        title: 'Interview HR',
-        date: '22/01/2024',
-        status: 'Passed',
-        details: [
-          {
-            attendee: 'Anna Yuliana, Candidate',
-            scheduleDate: '18/01/2024',
-            processDate: '24/01/2024',
-            processRemarks:
-              'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Fugit laborum quo labore culpa nam, pariatur numquam unde, voluptatibus expedita cumque assumenda voluptate. Animi rerum asperiores a officia corrupti ipsa debitis.',
-            document: '/sample.pdf',
-          },
-        ],
-      },
-      {
-        title: 'Interview User',
-        date: '22/01/2024',
-        status: 'Passed',
-        details: [
-          {
-            attendee: 'Anna Yuliana, Candidate, & User',
-            scheduleDate: '18/01/2024',
-            processDate: '24/01/2024',
-            processRemarks:
-              'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Fugit laborum quo labore culpa nam, pariatur numquam unde, voluptatibus expedita cumque assumenda voluptate. Animi rerum asperiores a officia corrupti ipsa debitis.',
-            document: '/sample.pdf',
-          },
-        ],
-      },
-      {
-        title: 'Technical Test',
-        date: '22/01/2024',
-        status: 'Failed',
-        details: [
-          {
-            attendee: 'Anna Yuliana, Candidate, & User',
-            scheduleDate: '18/01/2024',
-            processDate: '24/01/2024',
-            processRemarks:
-              'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Fugit laborum quo labore culpa nam, pariatur numquam unde, voluptatibus expedita cumque assumenda voluptate. Animi rerum asperiores a officia corrupti ipsa debitis.',
-            document: '/sample.pdf',
-          },
-        ],
-      },
-    ],
-  },
-]
+const statusConfig: Record<string, { Icon: LucideIcon; className: string }> = {
+  PASSED: { Icon: CheckCircle2Icon, className: 'text-green-600' },
+  FAILED: { Icon: XCircleIcon, className: 'text-error-600' },
+  WAITING: { Icon: ClockIcon, className: 'text-gray-600' },
+  PROCESS: { Icon: CircleEllipsisIcon, className: 'text-primary-600' },
+}
 
-const ViewHistoryModal: React.FC<OptionModalProps> = ({ show, onClose }) => {
-  const [showDetails, setShowDetails] = useState<boolean[]>([false, false, false])
+const Status: React.FC<{ status?: string }> = ({ status }) => {
+  status = status?.toUpperCase() || 'UNKNOWN'
+  const { Icon = AlertCircleIcon, className = 'text-warning-600' } = statusConfig[status]
+  return (
+    <div className={twJoin(className, 'flex justify-center gap-2')}>
+      <Icon size={18} />
+      <span className="text-sm font-semibold capitalize">{status.toLowerCase()}</span>
+    </div>
+  )
+}
 
-  const handleShowDetails = (index: number) => {
-    setShowDetails((prevDetails) => prevDetails.map((detail, idx) => (idx === index ? !detail : detail)))
-  }
+const ViewHistoryModal: React.FC<OptionModalProps> = ({ show, candidate: applicant, onClose }) => {
+  const [aplicantDataTable, setAplicantDataTable] = useState<ICandidate>()
+  const [aplicantDetail, setAplicantDetail] = useState<IApplicant | null>(null)
+  const [showDetailIndex, setShowDetailIndex] = useState<number | null>(null)
 
-  const handleOpenPdf = () => {
-    window.open('/sample.pdf', '_blank')
-  }
-
-  const renderStatus = (status: string) => {
-    if (status === 'Passed') {
-      return (
-        <div className="flex items-center gap-2">
-          <CheckCircle2Icon className="text-green-600" />
-          <span className="text-sm font-semibold text-green-600">Passed</span>
-        </div>
-      )
-    } else if (status === 'Failed') {
-      return (
-        <div className="flex items-center gap-2">
-          <XCircleIcon className="text-red-600" />
-          <span className="text-sm font-semibold text-red-600">Failed</span>
-        </div>
-      )
+  useEffect(() => {
+    console.log(applicant)
+    const controller = new AbortController()
+    const load = async (applicant: ICandidate, signal: AbortSignal) => {
+      const data = await processService.fetchDetailProcess(applicant.id, signal)
+      setAplicantDetail(data)
     }
-    return null
-  }
+
+    if (applicant?.id) {
+      setAplicantDataTable(applicant)
+      setAplicantDetail(null)
+      load(applicant, controller.signal)
+    }
+
+    return () => {
+      controller.abort()
+    }
+  }, [applicant])
 
   return (
-    <MainModal className="max-w-xl py-12" show={show} onClose={onClose}>
-      {data.map((item, index) => (
+    <MainModal className="max-w-xl py-12" show={!!show} onClose={onClose}>
+      <div className="mb-8">
+        <h4 className="mb-2 text-center text-2xl font-semibold">Candidate History</h4>
+        <p className="text-center">Candidate Process History for {aplicantDataTable?.name}</p>
+      </div>
+
+      {!aplicantDetail && (
+        <div className="flex items-center justify-center py-48">
+          <Spinner height={40} className="text-primary-600" />
+        </div>
+      )}
+
+      {aplicantDetail && (
         <>
-          <div key={index} className="mb-8">
-            <h4 className="mb-2 text-center text-2xl font-semibold">Candidate History</h4>
-            <p className="text-center">Candidate Process History for {item.candidate.name}</p>
-          </div>
           <div className="pb-2">
-            <h3 className="text-lg font-semibold">{item.jobApplication.position}</h3>
-            <p className="text-xs text-gray-500">{item.jobApplication.id}</p>
+            <h3 className="text-lg font-semibold">{aplicantDetail.vacancy?.name}</h3>
+            <p className="text-xs text-gray-500">{aplicantDetail.vacancy?.rrNumber}</p>
           </div>
 
-          <div className="p-3">
-            <ol className="border-l border-dashed">
-              {item.jobApplicationProcess.map((process, idx) => (
-                <li key={idx} className="relative mb-5 pl-6 last:mb-0">
-                  <span className="absolute left-[-0.4rem] top-1.5 flex h-3 w-3 items-center justify-center rounded-full bg-white ring-4 ring-primary-600" />
-                  <div className="flex items-center justify-between">
-                    <div>
-                      <h3 className="flex items-center gap-3 font-semibold">{process.title}</h3>
-                      <p className="mb-2 text-xs text-gray-500">{process.date}</p>
-                      <Button
-                        type="button"
-                        size="small"
-                        color="default"
-                        variant="light"
-                        className="text-xs"
-                        onClick={() => handleShowDetails(idx)}
-                      >
-                        Show Details
-                      </Button>
-                    </div>
-                    <div className="flex items-center gap-2">{renderStatus(process.status)}</div>
-                  </div>
-
-                  {showDetails[idx] && (
-                    <Card className="mt-4 px-2 py-4">
-                      <CardBody>
-                        <div className="mb-3">
-                          <h3 className="text-sm font-semibold">Attendee</h3>
-                          <span className="flex items-center gap-1 text-xs">
-                            <UserIcon size={16} />
-                            {process.details[0].attendee}
-                          </span>
-                        </div>
-                        <div className="mb-3">
-                          <h3 className="text-sm font-semibold">Action Scheduled</h3>
-                          <span className="flex items-center gap-1 text-xs">
-                            <CalendarIcon size={16} />
-                            {process.details[0].scheduleDate}
-                          </span>
-                        </div>
-                        <div className="mb-3">
-                          <h3 className="text-sm font-semibold">Process Date</h3>
-                          <span className="flex items-center gap-1 text-xs">
-                            <CalendarIcon size={16} />
-                            {process.details[0].processDate}
-                          </span>
-                        </div>
-                        <div className="mb-3">
-                          <h3 className="text-sm font-semibold">Process Remarks</h3>
-                          <span className="flex items-center gap-1 text-xs">{process.details[0].processRemarks}</span>
-                        </div>
-                        <h3 className="mb-1 text-sm font-semibold">Document</h3>
+          <>
+            <div className="p-3">
+              <ol className="border-l border-dashed">
+                {aplicantDetail.histories?.map((item, index) => (
+                  <li key={index} className="relative mb-5 pl-6 last:mb-0">
+                    <span className="absolute left-[-0.4rem] top-1.5 flex h-3 w-3 items-center justify-center rounded-full bg-white ring-4 ring-primary-600" />
+                    <div className="flex items-start justify-between gap-3">
+                      <div>
+                        <h3 className="flex gap-3 font-semibold">{item.applyProcess}</h3>
+                        <p className="mb-2 text-xs text-gray-500">{moment(item.processAt).format('D/M/Y HH:MM')}</p>
                         <Button
-                          onClick={handleOpenPdf}
+                          type="button"
                           size="small"
-                          color="primary"
-                          className="gap-2"
-                          variant="default"
-                          leftChild={<FileInputIcon size={18} />}
+                          color="default"
+                          variant="light"
+                          className="text-xs"
+                          onClick={() => setShowDetailIndex((v) => (v === index ? null : index))}
                         >
-                          <span className="text-xs">Result Attachment</span>
+                          {index === showDetailIndex ? 'Hide Details' : 'Show Details'}
                         </Button>
-                      </CardBody>
-                    </Card>
-                  )}
-                </li>
-              ))}
-            </ol>
-          </div>
+                      </div>
+                      <Status status={item.status} />
+                    </div>
+
+                    {showDetailIndex === index && (
+                      <Card className="mt-4 px-2 py-4">
+                        <CardBody>
+                          <div className="mb-3">
+                            <h3 className="text-sm font-semibold">Attendee</h3>
+                            <span className="flex items-center gap-1 text-xs">
+                              <UserIcon size={16} />
+                              {aplicantDetail.candidate?.name}
+                            </span>
+                          </div>
+                          {item.actionAt && (
+                            <div className="mb-3">
+                              <h3 className="text-sm font-semibold">Action Scheduled</h3>
+                              <span className="flex items-center gap-1 text-xs">
+                                <CalendarIcon size={16} />
+                                {moment(item.actionAt).format('D/M/Y HH:MM')}
+                              </span>
+                            </div>
+                          )}
+                          {item.processAt && (
+                            <div className="mb-3">
+                              <h3 className="text-sm font-semibold">Process Date</h3>
+                              <span className="flex items-center gap-1 text-xs">
+                                <CalendarIcon size={16} />
+                                {moment(item.processAt).format('D/M/Y HH:MM')}
+                              </span>
+                            </div>
+                          )}
+                          {item.notes && (
+                            <div className="mb-3">
+                              <h3 className="text-sm font-semibold">Process Remarks</h3>
+                              <span className="flex items-center gap-1 text-xs">{item.notes}</span>
+                            </div>
+                          )}
+                          {item.file && (
+                            <div className="mb-3">
+                              <h3 className="mb-1 text-sm font-semibold">Document</h3>
+                              <Button<'a'>
+                                as="a"
+                                href={item.file}
+                                target="_blank"
+                                size="small"
+                                color="primary"
+                                className="gap-2"
+                                variant="default"
+                                leftChild={<FileInputIcon size={18} />}
+                              >
+                                <span className="text-xs">Result Attachment</span>
+                              </Button>
+                            </div>
+                          )}
+                        </CardBody>
+                      </Card>
+                    )}
+                  </li>
+                ))}
+              </ol>
+            </div>
+          </>
         </>
-      ))}
+      )}
     </MainModal>
   )
 }
