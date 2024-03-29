057af2a5983a9a2706f91b63543b10be41dd9abf
Dede ardiansya <me@dedeard.my.id>
2024-03-29 12:43:15 +0800

    ✨feature: complete apply to page.

--- src/pages/payroll/apply-to/ApplyToPage.tsx
+++ src/pages/payroll/apply-to/ApplyToPage.tsx
@@ -7,9 +7,14 @@ import React, { useCallback, useEffect, useMemo, useState } from 'react'
 import EmployeeItem from './components/EmployeeItem'
 
 const ApplyToPage: React.FC = () => {
-  const [employees, setEmployees] = useState<IDataTableEmployee[]>([])
+  const [employees, setEmployees] = useState<IDataTableEmployee[]>()
   const [selected, setSelected] = useState<string[]>([])
   const [pageError, setPageError] = useState<any>()
+
+  const [search, setSearch] = useState({
+    selected: '',
+    unselected: '',
+  })
 
   useEffect(() => {
     const load = async () => {
@@ -40,73 +45,84 @@ const ApplyToPage: React.FC = () => {
     [employees, selected],
   )
 
+  const filteredSelected = useMemo(() => {
+    const filter = (search: string, data: IDataTableEmployee) => {
+      return (data.name?.toLowerCase() || '').replace(/\s+/g, '').includes(search.toLowerCase().replace(/\s+/g, ''))
+    }
+    return search.selected ? data.selected.filter((data) => filter(search.selected, data)) : data.selected
+  }, [data.selected, search.selected])
+
+  const filteredUnselected = useMemo(() => {
+    const filter = (search: string, data: IDataTableEmployee) => {
+      return (data.name?.toLowerCase() || '').replace(/\s+/g, '').includes(search.toLowerCase().replace(/\s+/g, ''))
+    }
+    return search.unselected ? data.unselected.filter((data) => filter(search.unselected, data)) : data.unselected
+  }, [data.unselected, search.unselected])
+
   if (pageError) throw pageError
 
   return (
     <>
       <PageHeader breadcrumb={[{ text: 'Payroll' }, { text: 'Apply To' }]} />
-      {!employees && (
-        <div className="flex items-center justify-center py-10">
-          <Spinner className="h-10 w-10 text-primary-600" />
-        </div>
-      )}
+
       <Container className="relative flex flex-col gap-3 py-3 xl:pb-8">
         <Card>
           <CardHeader className="font-semibold">Select Employee(s)</CardHeader>
           <CardBody className="grid grid-cols-2 gap-px bg-gray-200 p-0">
             <div className="bg-white">
-              <div className="flex items-center justify-between p-3">
-                <span className="block text-sm font-semibold">View 10 of 99 employees</span>
-                <button
-                  className="block text-sm text-primary-600 disabled:text-gray-400"
-                  disabled={!data.unselected.length}
-                  onClick={() => setSelected(employees.map((el) => el.oid))}
-                >
-                  Select All
-                </button>
-              </div>
-
-              <div className="px-3 pb-2">
-                <Input
-                  type="text"
-                  placeholder="Search..."
-                  className="peer m-0"
-                  inputClassName="peer pl-7"
-                  rightChild={
-                    <SearchIcon
-                      className="pointer-events-none absolute left-2 top-1/2 -translate-y-1/2 text-gray-400 peer-focus:text-primary-600"
-                      size={16}
-                    />
-                  }
-                />
-              </div>
-
               {!employees && (
                 <div className="flex items-center justify-center py-10">
                   <Spinner className="h-10 w-10 text-primary-600" />
                 </div>
               )}
-
               {employees && (
-                <ul className="flex flex-col divide-y border-t">
-                  {data.unselected
-                    .filter(({ oid }) => !selected.includes(oid))
-                    .map((el) => (
+                <>
+                  <div className="flex items-center justify-between p-3">
+                    <span className="block text-sm font-semibold">View {employees.length} total employees</span>
+                    <button
+                      className="block text-sm text-primary-600 disabled:text-gray-400"
+                      disabled={!data.unselected.length}
+                      onClick={() => setSelected(employees.map((el) => el.oid))}
+                    >
+                      Select All
+                    </button>
+                  </div>
+
+                  <div className="px-3 pb-2">
+                    <Input
+                      type="text"
+                      placeholder="Search..."
+                      className="peer m-0"
+                      inputClassName="peer pl-7"
+                      rightChild={
+                        <SearchIcon
+                          className="pointer-events-none absolute left-2 top-1/2 -translate-y-1/2 text-gray-400 peer-focus:text-primary-600"
+                          size={16}
+                        />
+                      }
+                      value={search.unselected}
+                      onChange={(e) => setSearch(({ selected }) => ({ selected, unselected: e.target.value }))}
+                    />
+                  </div>
+
+                  <ul className="flex flex-col divide-y border-t">
+                    {filteredUnselected.map((el) => (
                       <EmployeeItem key={el.oid} item={el} onClick={handleSelect} />
                     ))}
-                </ul>
-              )}
+                  </ul>
 
-              {employees && !data.unselected.length && (
-                <div className="py-6 text-center">
-                  <span className="mb-2 block text-xl">No available employees</span>
-                  <span className="block text-xs">Please select an employee from the list below.</span>
-                </div>
+                  {!data.unselected.length && (
+                    <div className="py-6 text-center">
+                      <span className="mb-2 block text-xl">No available employees</span>
+                      <span className="block text-xs">Please select an employee from the list below.</span>
+                    </div>
+                  )}
+                </>
               )}
             </div>
             <div className="bg-white">
               <div className="flex items-center justify-between p-3">
-                <span className="block text-sm font-semibold">0 employee(s) selected</span>
+                <span className="block text-sm font-semibold">{selected.length} employee(s) selected</span>
                 <button
                   className="block text-sm text-error-600 disabled:text-gray-400"
                   disabled={!data.selected.length}
@@ -128,24 +144,18 @@ const ApplyToPage: React.FC = () => {
                       size={16}
                     />
                   }
+                  value={search.selected}
+                  onChange={(e) => setSearch(({ unselected }) => ({ unselected, selected: e.target.value }))}
                 />
               </div>
 
-              {!employees && (
-                <div className="flex items-center justify-center py-10">
-                  <Spinner className="h-10 w-10 text-primary-600" />
-                </div>
-              )}
+              <ul className="flex flex-col divide-y border-t">
+                {filteredSelected.map((el) => (
+                  <EmployeeItem key={el.oid} item={el} onClick={handleUnselect} />
+                ))}
+              </ul>
 
-              {employees && (
-                <ul className="flex flex-col divide-y border-t">
-                  {data.selected.map((el) => (
-                    <EmployeeItem key={el.oid} item={el} onClick={handleUnselect} />
-                  ))}
-                </ul>
-              )}
-
-              {employees && !data.selected.length && (
+              {!data.selected.length && (
                 <div className="py-6 text-center">
                   <span className="mb-2 block text-xl">No selected employees</span>
                   <span className="block text-xs">You haven't selected any employees yet.</span>
