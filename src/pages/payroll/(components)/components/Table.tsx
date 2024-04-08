9757c2630248cbc39c78c5b4e21d1c8ffb8377ac
Dede Ariansya <me@dedeard.my.id>
2024-04-08 21:11:58 +0800

    🐞 fix: redurect page when component applied success fully.

--- src/pages/payroll/(components)/components/Table.tsx
+++ src/pages/payroll/(components)/components/Table.tsx
@@ -74,7 +74,7 @@ const Table: React.FC<PropTypes> = ({ type, items, loading, onRefresh }) => {
 
   return (
     <>
-      <ApplyToModal type={type} item={selectedToApply} onClose={() => setSelectedToApply(null)} onSubmited={onRefresh} />
+      <ApplyToModal type={type} item={selectedToApply} onClose={() => setSelectedToApply(null)} />
       <EditModal type={type} item={selectedToEdit} onClose={() => setSelectedToEdit(null)} onUpdated={onRefresh} />
       <MainTable headerItems={headerItems} bodyItems={bodyItems} loading={loading} />
     </>
