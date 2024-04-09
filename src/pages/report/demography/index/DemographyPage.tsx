8c1d858800d7c2a5c00131c8fe807820050219ec
Hafizh Pratama <hafizhpratama99@gmail.com>
2024-04-09 11:59:57 +0800

    fix: update page

--- src/pages/report/demography/index/DemographyPage.tsx
+++ src/pages/report/demography/index/DemographyPage.tsx
@@ -1,65 +1,34 @@
-import React from 'react'
 import Container from '@/components/Elements/Container'
 import PageHeader from '@/components/Elements/PageHeader'
-import { Chart as ChartJS, CategoryScale, LinearScale, BarElement, Title, Tooltip, Legend, PointElement, LineElement } from 'chart.js'
+import { Chart as ChartJS, ArcElement, Tooltip, Legend } from 'chart.js'
 import { BaseInputDate, Card, CardBody, Select } from 'jobseeker-ui'
-import { Bar, Line } from 'react-chartjs-2'
-import StatisticCards from '../../components/StatisticCards'
-import MainCardHeader from '@/components/Elements/MainCardHeader'
-import MainCard from '@/components/Elements/MainCard'
-import Table from '../../components/Table'
-import PageCard from '../../components/PageCard'
+import { Pie } from 'react-chartjs-2'
 
-ChartJS.register(CategoryScale, LinearScale, BarElement, Title, Tooltip, Legend, PointElement, LineElement)
-
-export const options = {
-  responsive: true,
-  plugins: {
-    legend: {
-      position: 'top' as const,
-    },
-    title: {
-      display: true,
-      text: 'Chart.js Bar Chart',
-    },
-  },
-}
-
-const labels = ['January', 'February', 'March', 'April', 'May', 'June', 'July']
+ChartJS.register(ArcElement, Tooltip, Legend)
 
 export const data = {
-  labels,
+  labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
   datasets: [
     {
-      label: 'Dataset 1',
+      label: '# of Votes',
       data: [12, 19, 3, 5, 2, 3],
-      backgroundColor: 'rgba(255, 99, 132, 0.5)',
-    },
-  ],
-}
-
-export const optionsLine = {
-  responsive: true,
-  plugins: {
-    legend: {
-      position: 'top' as const,
-    },
-    title: {
-      display: true,
-      text: 'Chart.js Line Chart',
-    },
-  },
-}
-
-const dataLine = {
-  labels,
-  datasets: [
-    {
-      label: 'Dataset 1',
-      data: [12, 19, 3, 5, 2, 3],
-      backgroundColor: 'rgba(255, 99, 132, 0.5)',
-      borderColor: 'rgba(255, 99, 132, 1)',
-      fill: false,
+      backgroundColor: [
+        'rgba(255, 99, 132, 0.2)',
+        'rgba(54, 162, 235, 0.2)',
+        'rgba(255, 206, 86, 0.2)',
+        'rgba(75, 192, 192, 0.2)',
+        'rgba(153, 102, 255, 0.2)',
+        'rgba(255, 159, 64, 0.2)',
+      ],
+      borderColor: [
+        'rgba(255, 99, 132, 1)',
+        'rgba(54, 162, 235, 1)',
+        'rgba(255, 206, 86, 1)',
+        'rgba(75, 192, 192, 1)',
+        'rgba(153, 102, 255, 1)',
+        'rgba(255, 159, 64, 1)',
+      ],
+      borderWidth: 1,
     },
   ],
 }
@@ -70,40 +39,62 @@ const DemographyPage: React.FC = () => {
       <PageHeader breadcrumb={[{ text: 'Report' }, { text: 'Candidate Demography' }]} title="Candidate Demography" />
 
       <Container className="py-3 xl:pb-8">
-        <StatisticCards />
-        <Card className="my-4 p-8">
+        <Card>
           <CardBody className="overflow-x-auto p-0 2xl:overflow-x-visible">
-            <div className="flex justify-end">
-              <BaseInputDate className="w-56" placeholder="Start - End Date" />
-            </div>
-            <Bar options={options} data={data} />
-          </CardBody>
-        </Card>
-        <MainCard header={() => <MainCardHeader title="User Activity" />} body={<Table items={[]} />} footer={[]} />
-        <Card className="my-4 p-8">
-          <CardBody className="overflow-x-auto p-0 2xl:overflow-x-visible">
-            <div className="flex flex-col">
-              <div className="flex items-center justify-end">
-                <Select className="w-40" options={[]} placeholder="2024" />
-
-                <div className="mb-2">
-                  <CardBody className="p-0">
-                    <div className="chrome-scrollbar flex gap-3 overflow-x-scroll p-3 pb-2">
-                      {['Attendance', 'Client Visit', 'Overtime'].map((label, index) => (
-                        <PageCard key={index} label={label} activeLabel={'Attendance'} />
-                      ))}
-                    </div>
-                  </CardBody>
+            <div className="relative z-10 rounded-t-lg border-b bg-white/80 p-4 backdrop-blur">
+              <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-3">
+                <div>
+                  <div className="w-full rounded-lg border p-4 text-center">
+                    <h2 className="mb-2 text-lg font-semibold">Province</h2>
+                    <BaseInputDate className="mb-2" placeholder="Start - End Date" />
+                    <Select placeholder="All Province" options={[]} className="mb-2" />
+                    <Pie data={data} />
+                  </div>
+                </div>
+                <div>
+                  <div className="w-full rounded-lg border p-4 text-center">
+                    <h2 className="mb-2 text-lg font-semibold">Age</h2>
+                    <BaseInputDate className="mb-2" placeholder="Start - End Date" />
+                    <Select placeholder="All Age" options={[]} className="mb-2" />
+                    <Pie data={data} />
+                  </div>
+                </div>
+                <div>
+                  <div className="w-full rounded-lg border p-4 text-center">
+                    <h2 className="mb-2 text-lg font-semibold">Education</h2>
+                    <BaseInputDate className="mb-2" placeholder="Start - End Date" />
+                    <Select placeholder="All Education" options={[]} className="mb-2" />
+                    <Pie data={data} />
+                  </div>
+                </div>
+                <div>
+                  <div className="w-full rounded-lg border p-4 text-center">
+                    <h2 className="mb-2 text-lg font-semibold">Gender</h2>
+                    <BaseInputDate className="mb-2" placeholder="Start - End Date" />
+                    <Select placeholder="All Gender" options={[]} className="mb-2" />
+                    <Pie data={data} />
+                  </div>
+                </div>
+                <div>
+                  <div className="w-full rounded-lg border p-4 text-center">
+                    <h2 className="mb-2 text-lg font-semibold">Department</h2>
+                    <BaseInputDate className="mb-2" placeholder="Start - End Date" />
+                    <Select placeholder="All Department" options={[]} className="mb-2" />
+                    <Pie data={data} />
+                  </div>
+                </div>
+                <div>
+                  <div className="w-full rounded-lg border p-4 text-center">
+                    <h2 className="mb-2 text-lg font-semibold">Experience</h2>
+                    <BaseInputDate className="mb-2" placeholder="Start - End Date" />
+                    <Select placeholder="All Experience" options={[]} className="mb-2" />
+                    <Pie data={data} />
+                  </div>
                 </div>
               </div>
-              <div className="flex items-center justify-end">
-                <Select className="w-64" options={[]} placeholder="All" />
-              </div>
             </div>
-            <Line options={optionsLine} data={dataLine} />
           </CardBody>
         </Card>
-        <MainCard header={() => <MainCardHeader title="Number of Hired" />} body={<Table items={[]} />} footer={[]} />
       </Container>
     </>
   )
