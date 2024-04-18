33ad32e5fe8b4d85e86217eab3305e9067a8a5f6
Dede Ariansya <me@dedeard.my.id>
2024-04-19 00:35:59 +0800

    ✨feature: create localstorage hooks.

---
+++ src/hooks/use-local-storage-state.ts
@@ -0,0 +1,27 @@
+import { useState, useEffect } from 'react'
+
+function useLocalStorageState<T>(key: string, initialValue: T): [T, React.Dispatch<React.SetStateAction<T>>] {
+  // Retrieve the value from localStorage if it exists
+  const [storedValue, setStoredValue] = useState<T>(() => {
+    try {
+      const item = window.localStorage.getItem(key)
+      return item ? JSON.parse(item) : initialValue
+    } catch (error) {
+      console.error(`Error retrieving ${key} from localStorage: `, error)
+      return initialValue
+    }
+  })
+
+  // Update the localStorage value whenever storedValue changes
+  useEffect(() => {
+    try {
+      window.localStorage.setItem(key, JSON.stringify(storedValue))
+    } catch (error) {
+      console.error(`Error setting ${key} to localStorage: `, error)
+    }
+  }, [key, storedValue])
+
+  return [storedValue, setStoredValue]
+}
+
+export default useLocalStorageState
