8353184e4bb4a4e795cd67d042d9996e8feb3135
Dede Ariansya <me@dedeard.my.id>
2024-04-29 16:58:47 +0800

    ✨feature: complete guest upload documents.

--- src/router/guest.routes.tsx
+++ src/router/guest.routes.tsx
@@ -4,7 +4,20 @@ const guestRoutes: RouteObject[] = [
   {
     path: 'guest',
     lazy: () => import('@/components/Layout/GuestLayout'),
-    children: [{ path: 'job/requisition', lazy: () => import('@/pages/guest/job/requisition/page') }],
+    children: [
+      {
+        path: 'job/requisition',
+        lazy: () => import('@/pages/guest/job/requisition/page'),
+      },
+      {
+        path: 'process/offering-letter/upload-documents',
+        lazy: () => import('@/pages/guest/process/offering-letter/upload-documents/page'),
+      },
+      {
+        path: 'process/offering-letter/preview',
+        lazy: () => import('@/pages/guest/process/offering-letter/preview/page'),
+      },
+    ],
   },
 ]
 
