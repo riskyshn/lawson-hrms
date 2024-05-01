698dfbd30a90c1dedbc9d9397f47237dc5c4fd35
Dede Ariansya <me@dedeard.my.id>
2024-05-01 16:58:42 +0800

    🐞 fix: sidebar mini.

--- src/components/Layout/Sidebar/Sidebar.tsx
+++ src/components/Layout/Sidebar/Sidebar.tsx
@@ -8,7 +8,7 @@ import { NavLink as Link, useNavigate } from 'react-router-dom'
 import { twJoin } from 'tailwind-merge'
 
 const Sidebar: React.FC = () => {
-  const { sidebarMini, toggleSidebarOpen } = useLayout()
+  const { sidebarMini, sidebarActive, toggleSidebarOpen } = useLayout()
   const links = useLinks([rootLinks, recruitmentLinks, hrisLinks, settingsLinks])
   const navigate = useNavigate()
 
@@ -25,19 +25,21 @@ const Sidebar: React.FC = () => {
       <SidebarContent className="chrome-scrollbar flex flex-col gap-3 py-3">
         <div className="px-3">
           <Button
-            className="flex w-full items-center justify-center gap-0 overflow-hidden text-nowrap px-0"
+            className="flex w-full items-center justify-center gap-0 overflow-hidden truncate text-nowrap px-0"
             color="primary"
             onClick={() => navigate('/job/management/create')}
-            rightChild={<PlusCircle className={twJoin(sidebarMini && 'lg:ml-0', 'ml-2')} size={16} />}
+            rightChild={<PlusCircle className={twJoin(sidebarMini && !sidebarActive && 'lg:ml-0', 'ml-2')} size={16} />}
           >
-            <span className={twJoin(sidebarMini && 'lg:hidden')}>Post a Job</span>
+            <span className={twJoin(sidebarMini && !sidebarActive && 'lg:hidden')}>Post a Job</span>
           </Button>
         </div>
 
         <div className="flex w-full flex-col gap-1">
           {links.map(({ items, title }, key) => (
             <div className="mb-2 flex w-full flex-col gap-1 px-3" key={key}>
-              {!!title && <span className={twJoin(sidebarMini && 'lg:hidden', 'block px-2 text-xs text-gray-500')}>{title}</span>}
+              {!!title && (
+                <span className={twJoin(sidebarMini && !sidebarActive && 'lg:hidden', 'block px-2 text-xs text-gray-500')}>{title}</span>
+              )}
               <div className="flex w-full flex-col gap-2">
                 {items.map(({ child, parent }, key) => (
                   <SidebarItem child={child} key={key} parent={parent} />
@@ -47,7 +49,7 @@ const Sidebar: React.FC = () => {
           ))}
         </div>
 
-        <div className={twJoin(sidebarMini && 'lg:hidden', 'mt-auto flex flex-col items-center justify-center p-3')}>
+        <div className={twJoin(sidebarMini && !sidebarActive && 'lg:hidden', 'mt-auto flex flex-col items-center justify-center p-3')}>
           <span className="block text-center text-xs text-gray-500">Powered by</span>
           <LogoFull height={32} />
         </div>
