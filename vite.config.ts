1ba98f1e3cc65e65f58baa831b0b5c46b6fa4e88
Dede Ariansya <me@dedeard.my.id>
2024-05-07 22:37:45 +0800

    ⏪ rollback: enable service worker.

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,7 @@ import process from 'node:process'
 import { fileURLToPath, URL } from 'url'
 import react from '@vitejs/plugin-react-swc'
 import { defineConfig } from 'vite'
-
-// import { VitePWA } from 'vite-plugin-pwa'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -47,78 +46,78 @@ export default defineConfig({
     react(),
     // for https dev server
     // mkcert(),
-    // VitePWA({
-    //   injectRegister: 'auto',
-    //   manifest: {
-    //     display: 'standalone',
-    //     icons: [
-    //       { sizes: '192x192', src: 'icons/192.png' },
-    //       { sizes: '512x512', src: 'icons/512.png' },
-    //     ],
-    //     name: 'HRMS BASIC',
-    //     short_name: 'HRMS',
-    //     start_url: '.',
-    //     theme_color: '#ffffff',
-    //   },
-    //   workbox: {
-    //     runtimeCaching: [
-    //       {
-    //         handler: 'CacheFirst',
-    //         options: {
-    //           cacheName: 'google-fonts-cache',
-    //           cacheableResponse: {
-    //             statuses: [0, 200],
-    //           },
-    //           expiration: {
-    //             maxAgeSeconds: 60 * 60 * 24 * 365, // <== 365 days
-    //             maxEntries: 10,
-    //           },
-    //         },
-    //         urlPattern: /^https:\/\/fonts\.googleapis\.com\/.*/i,
-    //       },
-    //       {
-    //         handler: 'CacheFirst',
-    //         options: {
-    //           cacheName: 'gstatic-fonts-cache',
-    //           cacheableResponse: {
-    //             statuses: [0, 200],
-    //           },
-    //           expiration: {
-    //             maxAgeSeconds: 60 * 60 * 24 * 365, // <== 365 days
-    //             maxEntries: 10,
-    //           },
-    //         },
-    //         urlPattern: /^https:\/\/fonts\.gstatic\.com\/.*/i,
-    //       },
-    //       {
-    //         handler: 'CacheFirst',
-    //         options: {
-    //           cacheName: 'unpkg-cache',
-    //           cacheableResponse: {
-    //             statuses: [0, 200],
-    //           },
-    //           expiration: {
-    //             maxAgeSeconds: 60 * 60 * 24 * 365, // <== 365 days
-    //             maxEntries: 10,
-    //           },
-    //         },
-    //         urlPattern: /^https:\/\/unpkg\.com\/.*/i,
-    //       },
+    VitePWA({
+      injectRegister: 'auto',
+      manifest: {
+        display: 'standalone',
+        icons: [
+          { sizes: '192x192', src: 'icons/192.png' },
+          { sizes: '512x512', src: 'icons/512.png' },
+        ],
+        name: 'HRMS BASIC',
+        short_name: 'HRMS',
+        start_url: '.',
+        theme_color: '#ffffff',
+      },
+      workbox: {
+        runtimeCaching: [
+          {
+            handler: 'CacheFirst',
+            options: {
+              cacheName: 'google-fonts-cache',
+              cacheableResponse: {
+                statuses: [0, 200],
+              },
+              expiration: {
+                maxAgeSeconds: 60 * 60 * 24 * 365, // <== 365 days
+                maxEntries: 10,
+              },
+            },
+            urlPattern: /^https:\/\/fonts\.googleapis\.com\/.*/i,
+          },
+          {
+            handler: 'CacheFirst',
+            options: {
+              cacheName: 'gstatic-fonts-cache',
+              cacheableResponse: {
+                statuses: [0, 200],
+              },
+              expiration: {
+                maxAgeSeconds: 60 * 60 * 24 * 365, // <== 365 days
+                maxEntries: 10,
+              },
+            },
+            urlPattern: /^https:\/\/fonts\.gstatic\.com\/.*/i,
+          },
+          {
+            handler: 'CacheFirst',
+            options: {
+              cacheName: 'unpkg-cache',
+              cacheableResponse: {
+                statuses: [0, 200],
+              },
+              expiration: {
+                maxAgeSeconds: 60 * 60 * 24 * 365, // <== 365 days
+                maxEntries: 10,
+              },
+            },
+            urlPattern: /^https:\/\/unpkg\.com\/.*/i,
+          },
 
-    //       {
-    //         handler: 'StaleWhileRevalidate',
-    //         options: {
-    //           cacheName: 'master-data',
-    //           expiration: {
-    //             maxAgeSeconds: 60 * 60 * 24 * 30, // <== 30 days
-    //             maxEntries: 20,
-    //           },
-    //         },
-    //         urlPattern: /^https:\/\/master\.api-jobseeker\.site\/.*/i,
-    //       },
-    //     ],
-    //   },
-    // }),
+          {
+            handler: 'StaleWhileRevalidate',
+            options: {
+              cacheName: 'master-data',
+              expiration: {
+                maxAgeSeconds: 60 * 60 * 24 * 30, // <== 30 days
+                maxEntries: 20,
+              },
+            },
+            urlPattern: /^https:\/\/master\.api-jobseeker\.site\/.*/i,
+          },
+        ],
+      },
+    }),
   ],
   resolve: {
     alias: [{ find: '@', replacement: fileURLToPath(new URL('./src', import.meta.url)) }],
