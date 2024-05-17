1695787ae09d47222eab100e519d02f1d7e1ddcf
Dede Ariansya <me@dedeard.my.id>
2024-05-17 15:26:50 +0800

    🐞 fix: swc.

--- vite.config.ts
+++ vite.config.ts
@@ -1,74 +1,19 @@
-// import mkcert from 'vite-plugin-mkcert'
-import type { VitePWAOptions } from 'vite-plugin-pwa'
 import process from 'node:process'
 import { fileURLToPath, URL } from 'url'
-import replace from '@rollup/plugin-replace'
-import react from '@vitejs/plugin-react-swc'
+import replace, { RollupReplaceOptions } from '@rollup/plugin-replace'
+import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
-import { VitePWA } from 'vite-plugin-pwa'
+import { VitePWA, VitePWAOptions } from 'vite-plugin-pwa'
+import manifest from './config/manifest'
+import rollupOptions from './config/rollupOptions'
+import workbox from './config/workbox'
 
 const pwaOptions: Partial<VitePWAOptions> = {
   mode: process.env.SW_DEV === 'true' ? 'development' : 'production',
   base: '/',
   includeAssets: ['favicon.svg'],
-
-  manifest: {
-    display: 'standalone',
-    icons: [
-      { sizes: '192x192', src: 'icons/192.png' },
-      { sizes: '512x512', src: 'icons/512.png' },
-    ],
-    name: 'HRMS BASIC',
-    short_name: 'HRMS',
-    start_url: '.',
-    theme_color: '#ffffff',
-  },
-  workbox: {
-    runtimeCaching: [
-      {
-        handler: 'CacheFirst',
-        options: {
-          cacheName: 'google-fonts-cache',
-          cacheableResponse: {
-            statuses: [0, 200],
-          },
-          expiration: {
-            maxAgeSeconds: 60 * 60 * 24 * 365, // <== 365 days
-            maxEntries: 10,
-          },
-        },
-        urlPattern: /^https:\/\/fonts\.googleapis\.com\/.*/i,
-      },
-      {
-        handler: 'CacheFirst',
-        options: {
-          cacheName: 'gstatic-fonts-cache',
-          cacheableResponse: {
-            statuses: [0, 200],
-          },
-          expiration: {
-            maxAgeSeconds: 60 * 60 * 24 * 365, // <== 365 days
-            maxEntries: 10,
-          },
-        },
-        urlPattern: /^https:\/\/fonts\.gstatic\.com\/.*/i,
-      },
-      {
-        handler: 'CacheFirst',
-        options: {
-          cacheName: 'unpkg-cache',
-          cacheableResponse: {
-            statuses: [0, 200],
-          },
-          expiration: {
-            maxAgeSeconds: 60 * 60 * 24 * 365, // <== 365 days
-            maxEntries: 10,
-          },
-        },
-        urlPattern: /^https:\/\/unpkg\.com\/.*/i,
-      },
-    ],
-  },
+  manifest,
+  workbox,
   devOptions: {
     enabled: process.env.SW_DEV === 'true',
     /* when using generateSW the PWA plugin will switch to classic */
@@ -77,7 +22,7 @@ const pwaOptions: Partial<VitePWAOptions> = {
   },
 }
 
-const replaceOptions = { __DATE__: new Date().toISOString() }
+const replaceOptions: RollupReplaceOptions = { __DATE__: new Date().toISOString(), preventAssignment: true }
 const claims = process.env.CLAIMS === 'true'
 const reload = process.env.RELOAD_SW === 'true'
 const selfDestroying = process.env.SW_DESTROY === 'true'
@@ -93,55 +38,15 @@ if (process.env.SW === 'true') {
 }
 
 if (claims) pwaOptions.registerType = 'autoUpdate'
-
-if (reload) {
-  // @ts-expect-error just ignore
-  replaceOptions.__RELOAD_SW__ = 'true'
-}
-
+if (reload) replaceOptions.__RELOAD_SW__ = 'true'
 if (selfDestroying) pwaOptions.selfDestroying = selfDestroying
 
-// https://vitejs.dev/config/
 export default defineConfig({
   build: {
-    rollupOptions: {
-      output: {
-        manualChunks: {
-          'chart.js': ['chart.js', 'react-chartjs-2'],
-          'core-packages': [
-            'react',
-            'react-dom',
-            'react-router-dom',
-            'tailwind-merge',
-            'js-cookie',
-            '@headlessui/react',
-            'zustand',
-            'moment',
-            'axios',
-          ],
-          'hook-form': ['yup', 'react-hook-form', '@hookform/resolvers'],
-          'jobseeker-ui': ['dequal', 'dayjs', 'react-tailwindcss-datepicker', 'jobseeker-ui'],
-          fullcalendar: [
-            '@fullcalendar/core',
-            '@fullcalendar/daygrid',
-            '@fullcalendar/interaction',
-            '@fullcalendar/list',
-            '@fullcalendar/react',
-            '@fullcalendar/timegrid',
-          ],
-          leaflet: ['leaflet', 'react-leaflet'],
-        },
-      },
-    },
+    rollupOptions,
     sourcemap: process.env.SOURCE_MAP === 'true',
   },
-  plugins: [
-    react(),
-    // for https dev server
-    // mkcert(),
-    VitePWA(pwaOptions),
-    replace(replaceOptions),
-  ],
+  plugins: [react(), VitePWA(pwaOptions), replace(replaceOptions)],
   resolve: {
     alias: [{ find: '@', replacement: fileURLToPath(new URL('./src', import.meta.url)) }],
   },
